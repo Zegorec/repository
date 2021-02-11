182d93120f210654fb98a2df0904d0b4de20980c
Zegorec <gorszeg@gmail.com>
2021-02-11 23:28:38 +0700

    placed function in eventListner

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const buttonAddTarget = document.querySelector('.buttonAdd');
 const inputTextTarget = document.querySelector('.inputText');
 //
+let showTextTargetLog = function () {
+  console.log(inputTextTarget.value)
+};
 
-buttonAddTarget.addEventListener('click', function () {
-  console.log(inputTextTarget.value);
-});
+buttonAddTarget.addEventListener('click', showTextTargetLog);
