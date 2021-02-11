3be3fab91ea0caf80d77de76a2a825bdd102dee0
Zegorec <gorszeg@gmail.com>
2021-02-11 23:36:18 +0700

    new function =>

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const buttonAddTarget = document.querySelector('.buttonAdd');
 const inputTextTarget = document.querySelector('.inputText');
 //
-let showTextTargetLog = function () {
-  console.log(inputTextTarget.value)
-};
+const showTextTargetLog = () => console.log(inputTextTarget.value);
 
 buttonAddTarget.addEventListener('click', showTextTargetLog);
