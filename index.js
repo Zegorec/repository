74edd71d5498a8f104c5914bf78af838caa01e1b
Zegorec <gorszeg@gmail.com>
2021-02-11 23:01:10 +0700

    i love addEventListner

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const buttonAddTarget = document.querySelector('.buttonAdd');
 const inputTextTarget = document.querySelector('.inputText');
 //
 
-buttonAddTarget.addEventListener('click', function() {
+buttonAddTarget.addEventListener('click', function () {
   console.log(inputTextTarget.value);
-})
+});
