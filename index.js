a85b780f7b1f969f76d8eb942d9316f20f433e05
Zegorec <gorszeg@gmail.com>
2021-02-12 23:33:30 +0700

    Add button delte

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const buttonAddTarget = document.querySelector('.buttonAdd');
 const inputTextTarget = document.querySelector('.inputText');
-const listTarget = document.querySelector('.listTarget')
+const listTarget = document.querySelector('.listTarget');
 //
-const showTextTargetLog = () => console.log(inputTextTarget.value);
+const proverka = () => console.log('rabotaet');
 const addTarget = () => {
   const newElement = document.createElement('div');
   newElement.textContent = inputTextTarget.value;
+  const newButton = document.createElement('button');
+  newButton.textContent = 'Удалить';
+  newButton.classList.add = 'buttonForDelte';
+  newElement.appendChild(newButton);
   inputTextTarget.value = '';
   return listTarget.appendChild(newElement);
-} 
+};
 
 buttonAddTarget.addEventListener('click', addTarget);
