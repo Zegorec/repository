663add9f0df6992ad3a2cba7f0354298ccc072af
Zegorec <gorszeg@gmail.com>
2021-02-13 00:21:09 +0700

    don't work button delte on new elements

--- index.js
+++ index.js
@@ -5,13 +5,22 @@ const listTarget = document.querySelector('.listTarget');
 const proverka = () => console.log('rabotaet');
 const addTarget = () => {
   const newElement = document.createElement('div');
+  newElement.classList.add('target');
   newElement.textContent = inputTextTarget.value;
   const newButton = document.createElement('button');
+  newButton.classList.add('buttonForDelte');
   newButton.textContent = 'Удалить';
-  newButton.classList.add = 'buttonForDelte';
   newElement.appendChild(newButton);
   inputTextTarget.value = '';
   return listTarget.appendChild(newElement);
 };
 
 buttonAddTarget.addEventListener('click', addTarget);
+
+const divTarget = listTarget.querySelectorAll('.target');
+const buttonForDelte = listTarget.querySelectorAll('.buttonForDelte');
+const delteTarget = (elem) => elem.remove();
+
+for (let i = 0; i < divTarget.length; i++) {
+  buttonForDelte[i].addEventListener('click', proverka);
+}
